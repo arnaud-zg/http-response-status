711ea2aaba7ee32fd6e651808431342b0fa2a9cc
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-08-18 17:36:08 +0200

    chore(test): add test on NHttpHeaders

--- test/constants/http.test.ts
+++ test/constants/http.test.ts
@@ -1,7 +1,13 @@
-import { NHttpStatuses } from '../../src/constants/http';
+import { NHttpHeaders, NHttpStatuses } from '../../src/constants/http';
+
+describe('NHttpHeaders', () => {
+  it('should make a snapshot of NHttpHeaders', () => {
+    expect(NHttpHeaders).toMatchSnapshot();
+  });
+});
 
 describe('NHttpStatuses', () => {
-  it('should make a package of NHttpStatuses', () => {
+  it('should make a snapshot of NHttpStatuses', () => {
     expect(NHttpStatuses).toMatchSnapshot();
   });
 });
