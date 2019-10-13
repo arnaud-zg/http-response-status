61d79429fcce22451e744d9aa48cbfca87346d3a
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-10-13 19:33:03 +0200

    chore(project): remove pretty quick and add lint command

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-namespace': 0,
+  },
+};
