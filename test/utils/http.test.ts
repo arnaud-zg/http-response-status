02ba475dfd4c180506e15e0d834d6ccadb65b2dc
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-11-01 13:03:24 +0100

    test(unit): use getHttpStatusCategory for unit test

--- test/utils/http.test.ts
+++ test/utils/http.test.ts
@@ -1,4 +1,13 @@
-import { getHttpStatusName, getHttpStatusCategory } from '../../src/utils/http';
+import { NHttpStatuses } from '../../src/constants/http';
+import {
+  getHttpStatusCategory,
+  getHttpStatusName,
+  HTTP_STATUS_CLIENT_ERROR,
+  HTTP_STATUS_INFORMATIONAL,
+  HTTP_STATUS_REDIRECTION,
+  HTTP_STATUS_SERVER_ERROR,
+  HTTP_STATUS_SUCCESS,
+} from '../../src/utils/http';
 
 describe('getHttpStatusName', () => {
   it('should return nothing', () => {
@@ -70,7 +79,35 @@ describe('getHttpStatusName', () => {
     [511],
     [599],
   ])('get name when httpStatusCode is equal to %i', httpStatusCode => {
-    expect(getHttpStatusName(httpStatusCode)).toMatchSnapshot();
+    switch (getHttpStatusCategory(httpStatusCode)) {
+      case HTTP_STATUS_INFORMATIONAL:
+        expect(getHttpStatusName(httpStatusCode)).toEqual(
+          NHttpStatuses.EInformational[httpStatusCode]
+        );
+        break;
+      case HTTP_STATUS_SUCCESS:
+        expect(getHttpStatusName(httpStatusCode)).toEqual(
+          NHttpStatuses.ESuccess[httpStatusCode]
+        );
+        break;
+      case HTTP_STATUS_REDIRECTION:
+        expect(getHttpStatusName(httpStatusCode)).toEqual(
+          NHttpStatuses.ERedirection[httpStatusCode]
+        );
+        break;
+      case HTTP_STATUS_CLIENT_ERROR:
+        expect(getHttpStatusName(httpStatusCode)).toEqual(
+          NHttpStatuses.ClientError[httpStatusCode]
+        );
+        break;
+      case HTTP_STATUS_SERVER_ERROR:
+        expect(getHttpStatusName(httpStatusCode)).toEqual(
+          NHttpStatuses.EServerError[httpStatusCode]
+        );
+        break;
+      default:
+        break;
+    }
   });
 });
 
@@ -81,69 +118,69 @@ describe('getHttpStatusCategory', () => {
 
   test.each`
     httpStatusCode | expected
-    ${100}         | ${'Informational'}
-    ${101}         | ${'Informational'}
-    ${102}         | ${'Informational'}
-    ${200}         | ${'Success'}
-    ${201}         | ${'Success'}
-    ${202}         | ${'Success'}
-    ${203}         | ${'Success'}
-    ${204}         | ${'Success'}
-    ${205}         | ${'Success'}
-    ${206}         | ${'Success'}
-    ${207}         | ${'Success'}
-    ${208}         | ${'Success'}
-    ${226}         | ${'Success'}
-    ${300}         | ${'Redirection'}
-    ${301}         | ${'Redirection'}
-    ${302}         | ${'Redirection'}
-    ${303}         | ${'Redirection'}
-    ${304}         | ${'Redirection'}
-    ${305}         | ${'Redirection'}
-    ${307}         | ${'Redirection'}
-    ${308}         | ${'Redirection'}
-    ${400}         | ${'ClientError'}
-    ${401}         | ${'ClientError'}
-    ${402}         | ${'ClientError'}
-    ${405}         | ${'ClientError'}
-    ${404}         | ${'ClientError'}
-    ${405}         | ${'ClientError'}
-    ${406}         | ${'ClientError'}
-    ${407}         | ${'ClientError'}
-    ${408}         | ${'ClientError'}
-    ${409}         | ${'ClientError'}
-    ${410}         | ${'ClientError'}
-    ${411}         | ${'ClientError'}
-    ${412}         | ${'ClientError'}
-    ${413}         | ${'ClientError'}
-    ${414}         | ${'ClientError'}
-    ${415}         | ${'ClientError'}
-    ${416}         | ${'ClientError'}
-    ${417}         | ${'ClientError'}
-    ${418}         | ${'ClientError'}
-    ${421}         | ${'ClientError'}
-    ${422}         | ${'ClientError'}
-    ${423}         | ${'ClientError'}
-    ${424}         | ${'ClientError'}
-    ${426}         | ${'ClientError'}
-    ${428}         | ${'ClientError'}
-    ${429}         | ${'ClientError'}
-    ${431}         | ${'ClientError'}
-    ${444}         | ${'ClientError'}
-    ${451}         | ${'ClientError'}
-    ${499}         | ${'ClientError'}
-    ${500}         | ${'ServerError'}
-    ${501}         | ${'ServerError'}
-    ${502}         | ${'ServerError'}
-    ${503}         | ${'ServerError'}
-    ${504}         | ${'ServerError'}
-    ${505}         | ${'ServerError'}
-    ${506}         | ${'ServerError'}
-    ${507}         | ${'ServerError'}
-    ${508}         | ${'ServerError'}
-    ${510}         | ${'ServerError'}
-    ${511}         | ${'ServerError'}
-    ${599}         | ${'ServerError'}
+    ${100}         | ${HTTP_STATUS_INFORMATIONAL}
+    ${101}         | ${HTTP_STATUS_INFORMATIONAL}
+    ${102}         | ${HTTP_STATUS_INFORMATIONAL}
+    ${200}         | ${HTTP_STATUS_SUCCESS}
+    ${201}         | ${HTTP_STATUS_SUCCESS}
+    ${202}         | ${HTTP_STATUS_SUCCESS}
+    ${203}         | ${HTTP_STATUS_SUCCESS}
+    ${204}         | ${HTTP_STATUS_SUCCESS}
+    ${205}         | ${HTTP_STATUS_SUCCESS}
+    ${206}         | ${HTTP_STATUS_SUCCESS}
+    ${207}         | ${HTTP_STATUS_SUCCESS}
+    ${208}         | ${HTTP_STATUS_SUCCESS}
+    ${226}         | ${HTTP_STATUS_SUCCESS}
+    ${300}         | ${HTTP_STATUS_REDIRECTION}
+    ${301}         | ${HTTP_STATUS_REDIRECTION}
+    ${302}         | ${HTTP_STATUS_REDIRECTION}
+    ${303}         | ${HTTP_STATUS_REDIRECTION}
+    ${304}         | ${HTTP_STATUS_REDIRECTION}
+    ${305}         | ${HTTP_STATUS_REDIRECTION}
+    ${307}         | ${HTTP_STATUS_REDIRECTION}
+    ${308}         | ${HTTP_STATUS_REDIRECTION}
+    ${400}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${401}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${402}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${405}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${404}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${405}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${406}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${407}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${408}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${409}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${410}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${411}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${412}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${413}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${414}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${415}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${416}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${417}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${418}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${421}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${422}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${423}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${424}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${426}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${428}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${429}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${431}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${444}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${451}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${499}         | ${HTTP_STATUS_CLIENT_ERROR}
+    ${500}         | ${HTTP_STATUS_SERVER_ERROR}
+    ${501}         | ${HTTP_STATUS_SERVER_ERROR}
+    ${502}         | ${HTTP_STATUS_SERVER_ERROR}
+    ${503}         | ${HTTP_STATUS_SERVER_ERROR}
+    ${504}         | ${HTTP_STATUS_SERVER_ERROR}
+    ${505}         | ${HTTP_STATUS_SERVER_ERROR}
+    ${506}         | ${HTTP_STATUS_SERVER_ERROR}
+    ${507}         | ${HTTP_STATUS_SERVER_ERROR}
+    ${508}         | ${HTTP_STATUS_SERVER_ERROR}
+    ${510}         | ${HTTP_STATUS_SERVER_ERROR}
+    ${511}         | ${HTTP_STATUS_SERVER_ERROR}
+    ${599}         | ${HTTP_STATUS_SERVER_ERROR}
   `(
     'returns $expected when httpStatusCode is equal to $httpStatusCode',
     ({ httpStatusCode, expected }) => {
